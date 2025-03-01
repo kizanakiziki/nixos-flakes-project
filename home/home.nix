2a2d83490180ed94c4acfa0735fe3e4855a0cf27
kizanakiziki-nixos-flakes <kizanakiziki@gmail.com>
2025-03-02 03:43:02 +0700

    change modules folder in home to neovim

--- home/home.nix
+++ home/home.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     # ./packages.nix
-    ./modules/neovim.nix
+    ./neovim/neovim.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
